dbf3f9ccb53af1026c669de3fdc2a92331b2d1e0
t1nk-r <12009110+gusztavj@users.noreply.github.com>
2024-02-03 15:02:13 +0100

    Improved code and docs aesthetics

--- __init__.py
+++ __init__.py
@@ -25,7 +25,7 @@
 #
 # ** MIT License **
 # 
-# Copyright (c) 2023, T1nk-R (Gusztáv Jánvári)
+# Copyright (c) 2023-2024, T1nk-R (Gusztáv Jánvári)
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
 # (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, 
